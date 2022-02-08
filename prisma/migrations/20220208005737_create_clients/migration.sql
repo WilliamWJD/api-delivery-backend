e7c0fc01162b60a8d9acb863a1e04987952671f2
williamwjd <william.jose.wjd@gmail.com>
2022-02-07 22:04:54 -0300

    creating migrations

---
+++ prisma/migrations/20220208005737_create_clients/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Clients" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "Clients_pkey" PRIMARY KEY ("id")
+);
---
+++ prisma/migrations/20220208005821_change_table_name/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Clients` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Clients";
+
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "clients_pkey" PRIMARY KEY ("id")
+);
